65ed46244af6c75af55d3025a91cfffe742ce843
Ayşegül AKPINARLI <94296524+aayazakpinarli@users.noreply.github.com>
2025-11-15 20:21:37 +0300

    Truncating functionality for admin is added

---
+++ BookStoreProject/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using APP.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+public class AdminController : Controller
+{
+    private readonly DbContext _context;
+
+    public AdminController(DbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Truncate()
+    {
+        var helper = new DatabaseHelper(_context);
+        helper.TruncateAllTables();
+        return Content("Database truncated!");
+    }
+}
