44912af40d653478c294601163959f8129078a7a
Ayşegül AKPINARLI <94296524+aayazakpinarli@users.noreply.github.com>
2025-12-22 12:07:57 +0300

    Cart Item models created

---
+++ APP/Models/CartItem.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace APP.Models
+{
+    public class CartItem
+    {
+        public int UserId { get; set; }
+
+        public int BookId { get; set; }
+
+        [DisplayName("Book Name")]
+        public string BookName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        [DisplayName("Unit Price")]
+        public string UnitPriceF { get; set; }
+    }
+}
---
+++ APP/Models/CartItemGroupedBy.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace APP.Models
+{
+    public class CartItemGroupedBy
+    {
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        [DisplayName("Book Name")]
+        public string BookName { get; set; }
+        [DisplayName("Book Count")]
+        public int BookCount { get; set; }
+        public decimal TotalPrice { get; set; }
+        [DisplayName("Total Price")]
+        public string TotalPriceF { get; set; }
+    }
+}
