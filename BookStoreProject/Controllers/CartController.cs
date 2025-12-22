47d30203e50acd84ea37e64e791fb64810abc36e
Ayşegül AKPINARLI <94296524+aayazakpinarli@users.noreply.github.com>
2025-12-22 12:08:11 +0300

    Cart Controller created

---
+++ BookStoreProject/Controllers/CartController.cs
@@ -0,0 +1,48 @@
+﻿using APP.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreProject.Controllers
+{
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly ICartService _cartService;
+
+        public CartController(ICartService cartService)
+        {
+            _cartService = cartService;
+        }
+
+        private int GetUserId() => Convert.ToInt32(User.Claims.SingleOrDefault(c => c.Type == "Id")?.Value);
+
+        public IActionResult Index()
+        {
+            var cartGroupedBy = _cartService.GetCartGroupedBy(GetUserId());
+            return View(cartGroupedBy);
+        }
+
+        public IActionResult Clear()
+        {
+            _cartService.ClearCart(GetUserId());
+            TempData["Message"] = "Cart cleared.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int BookId)
+        {
+            _cartService.RemoveFromCart(GetUserId(), BookId);
+            TempData["Message"] = "Book removed from cart.";
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Add(int BookId)
+        {
+            Console.WriteLine(BookId);
+            Console.WriteLine("HERE ISSS BOOK ID IN CONTROLLER ADD!!");
+            _cartService.AddToCart(GetUserId(), BookId);
+            TempData["Message"] = "Book added to cart.";
+            return RedirectToAction("Index", "");
+        }
+    }
+}
